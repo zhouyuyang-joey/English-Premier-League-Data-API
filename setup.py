667999e344b67ffe1f71d02c812e1f620f79aca2
zhouyuyang-joey <176059605+zhouyuyang-joey@users.noreply.github.com>
2025-06-16 16:19:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,44 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
 setup(
     name="eplda",
     version="0.2.1",
-    description="An unofficial API handler to footballapi.pulselive.com. Permitted for study purposes only.",
-    long_description=open("README.md", encoding="utf-8").read(),
+    description="An unofficial lightweight Python client library for Premier League Data API",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Zhou Yuyang",
     author_email="joeyjoeyjoemail@163.com",
     url="https://github.com/zhouyuyang-joey/English-Premier-League-Data-API",
     packages=find_packages(),
-    install_requires=[
-        "requests",
-        "pandas"
-    ],
+    include_package_data=True,
+    package_data={
+        'eplda': ['config.yaml'],
+    },
+    install_requires=requirements,
+    extras_require={
+        'dev': ['pytest', 'pytest-cov', 'black', 'flake8'],
+        'test': ['pytest', 'pytest-mock']
+    },
     python_requires=">=3.7",
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Sports/Football",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    keywords="premier-league football soccer api data statistics",
+    project_urls={
+        "Bug Reports": "https://github.com/zhouyuyang-joey/English-Premier-League-Data-API/issues",
+        "Source": "https://github.com/zhouyuyang-joey/English-Premier-League-Data-API",
+        "Documentation": "https://github.com/zhouyuyang-joey/English-Premier-League-Data-API/blob/main/README.md",
+    },
 )
