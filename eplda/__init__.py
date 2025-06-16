e09ba084be5ee32918ce55ff4b09eafc31b0d40c
zhouyuyang-joey <176059605+zhouyuyang-joey@users.noreply.github.com>
2025-06-16 15:02:10 +0100

    Update __init__.py

--- eplda/__init__.py
+++ eplda/__init__.py
@@ -3,4 +3,4 @@ from .config import config
 from .constants import StatTypes, APIEndpoints, DataKeys, OutputFormats, get_all_club_stat_types, validate_stat_type
 
 __author__ = 'Zhou Yuyang'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
